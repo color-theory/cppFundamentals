217346b8c8a34345b6118532f13ace0d905de834
subcogitate <jgstringer@gmail.com>
2017-11-02 18:08:36 +0000

    A2 - adding input from iostream ints

---
+++ A2-iostream/input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x, y;
+  cin >> x;
+
+  cout << "\nx is " << x << "\n\n";
+
+  cin >> x >> y;
+  
+  cout << "\nx is " << x << " and y is " << y << "\n\n";
+
+  return 0;
+}
