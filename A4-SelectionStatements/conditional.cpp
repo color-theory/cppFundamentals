b4c221478e302d9b33ae8925eec6b11d0b4353c4
subcogitate <jgstringer@gmail.com>
2017-11-03 03:24:31 +0000

    A4 - Added if check to conditional.cpp

--- A4-SelectionStatements/conditional.cpp
+++ A4-SelectionStatements/conditional.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
+  if(2 > 1)
+    {
+      cout << "2 is greater than 1";
+    }
+  else
+    {
+      cout << "2 is NOT greater than 1";
+    }
+  
   return 0;
 }
