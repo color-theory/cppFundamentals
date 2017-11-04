c70ea10cfba2affd7e18e5fa320a947929ce63c6
subcogitate <jgstringer@gmail.com>
2017-11-04 16:01:48 +0000

    A5 - Adding for loops

---
+++ A5-IteratorStatements/for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //for(initialize; test; update){statement}
+  for(int counter = 1; counter <= 5; ++counter){
+    cout << counter;
+  }
+
+  cout << "\n\n";
+  return 0;
+}
