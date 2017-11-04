64c5d7d1c20312d779f056e7dd30c2dfe836cd43
subcogitate <jgstringer@gmail.com>
2017-11-04 22:30:14 +0000

    A5- weird for loop stuff

--- A5-IteratorStatements/for.cpp
+++ A5-IteratorStatements/for.cpp
@@ -3,11 +3,43 @@ using namespace std;
 
 int main()
 {
-  //for(initialize; test; update){statement}
+  /*
+    
+    initialize
+    while(test)
+    {
+      statement
+      update
+    }
+
+    for(initialize; test; update){statement}
+
+  */
+  
   for(int counter = 1; counter <= 5; ++counter){
+    if(counter == 4)
+      {
+	continue;
+      }
     cout << counter;
   }
 
+  //for loops can have multiple initilizations and updates.
+  for(int x = 0, y = 3; (x < 3) || (y < 10); x++, y++)
+    {
+      cout << x << " " << y << "\n";
+    }
+
+  //for loops can omit any step
+  int inputValue;
+  for(;(cin >> inputValue);)
+    {
+      if(inputValue == 42)
+	{
+	  break;
+	}
+    }
+      
   cout << "\n\n";
   return 0;
 }
