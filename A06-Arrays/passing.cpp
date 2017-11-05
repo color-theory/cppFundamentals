29aca8ad6010a3125b8dcc06acd356a3b3e5c085
subcogitate <jgstringer@gmail.com>
2017-11-05 17:31:01 +0000

    Renaming folders for sorting purposes

--- A06-Arrays/passing.cpp
+++ A06-Arrays/passing.cpp
@@ -10,6 +10,8 @@ int secondItem(int myArray[])
 int main()
 {
   int newArray[4] = {1,2,3,4};
+
+  // the order of expression evaluation is unspecified. don't do this.
   cout << secondItem(newArray) << endl << newArray[1] << endl; // 42 2 
   cout << newArray[1] << endl; // 42
   return 0;
